3da066578c73fed1a1567d73ae96a5af624cefb9
Barsy-Barsevich <barsotion@yandex.ru>
2025-04-17 19:03:04 +0500

    usart rx tx transaction & example

--- src/usart_transaction.c
+++ src/usart_transaction.c
@@ -5,34 +5,67 @@
 HAL_Status_t usart_transaction_init(usart_transaction_t *trans, usart_transaction_cfg_t *cfg)
 {
     trans->host = cfg->host;
-    HAL_DMA_Request_t req = DMA_USART_0_REQUEST;
-    if (cfg->host == UART_1) req = DMA_USART_1_REQUEST;
-    HAL_DMA_Config_t dma_cfg = {
-        .priority = cfg->dma_priority,
-        .read_mode = DMA_MEMORY_MODE,
-        .write_mode = DMA_PERIPHERY_MODE,
-        .read_increment = 1,
-        .write_increment = 0,
-        .read_block_size = DMA_BLOCK_8BIT,
-        .write_block_size = DMA_BLOCK_8BIT,
-        .read_burst_size = 0,
-        .write_burst_size = 0,
-        .read_request = req,
-        .write_request = req,
-        .read_ack_en = 0,
-        .write_ack_en = 1,
-        .irq_en = 0,
-        .channel = cfg->dma_channel,
-        .src_address = 0,
-        .dst_address = (uint32_t)&(trans->host->TXDATA),
-        .transaction_len = 0,
-    };
+    trans->direction = cfg->direction;
+
+    HAL_DMA_Config_t dma_cfg = {0};
+    dma_cfg.priority = cfg->dma_priority;
+    dma_cfg.read_block_size = DMA_BLOCK_8BIT;
+    dma_cfg.write_block_size = DMA_BLOCK_8BIT;
+    dma_cfg.read_burst_size = 0;
+    dma_cfg.write_burst_size = 0;
+
+    if (cfg->host == UART_0)
+    {
+        dma_cfg.read_request = DMA_USART_0_REQUEST;
+        dma_cfg.write_request = DMA_USART_0_REQUEST;
+    }
+    else if (cfg->host == UART_1)
+    {
+        dma_cfg.read_request = DMA_USART_1_REQUEST;
+        dma_cfg.write_request = DMA_USART_1_REQUEST;
+    }
+    else return HAL_DMA_INCORRECT_ARGUMENT;
+
+    //TODO: post_cb as in esp-idf
+    dma_cfg.irq_en = 0;
+
+    if (cfg->direction == USART_TRANSACTION_TRANSMIT)
+    {
+
+        dma_cfg.read_mode = DMA_MEMORY_MODE;
+        dma_cfg.write_mode = DMA_PERIPHERY_MODE;
+        dma_cfg.read_increment = 1;
+        dma_cfg.write_increment = 0;
+        dma_cfg.read_ack_en = 0;
+        dma_cfg.write_ack_en = 1;
+        dma_cfg.src_address = 0;
+        dma_cfg.dst_address = (uint32_t)&(trans->host->TXDATA);
+    }
+    else if (cfg->direction == USART_TRANSACTION_RECEIVE)
+    {
+        dma_cfg.read_mode = DMA_PERIPHERY_MODE;
+        dma_cfg.write_mode = DMA_MEMORY_MODE;
+        dma_cfg.read_increment = 0;
+        dma_cfg.write_increment = 1;
+        dma_cfg.read_ack_en = 1;
+        dma_cfg.write_ack_en = 0;
+        dma_cfg.src_address = (uint32_t)&(trans->host->RXDATA);
+        dma_cfg.dst_address = 0;
+    }
+    else return HAL_DMA_INCORRECT_ARGUMENT;
+    dma_cfg.channel = cfg->dma_channel,
+    dma_cfg.transaction_len = 0;
+    
     return dma_transaction_init(&(trans->dma_transaction), &dma_cfg);
 }
 
-void RAM_ATTR usart_transaction_start(usart_transaction_t *trans, char *src, uint32_t len)
+void RAM_ATTR usart_transaction_start(usart_transaction_t *trans, char *arr, uint32_t len)
 {
-    trans->dma_transaction.config.SRC = (uint32_t)src;
+    if (trans->direction == USART_TRANSACTION_TRANSMIT)
+        trans->dma_transaction.config.SRC = (uint32_t)arr;
+    else if (trans->direction == USART_TRANSACTION_RECEIVE)
+        trans->dma_transaction.config.DST = (uint32_t)arr;
+    else return;
     trans->dma_transaction.config.LEN = len;
     dma_transaction_start(&(trans->dma_transaction));
 }
