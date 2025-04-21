9e8751816e14c3e7d80ea1dedd3b379897738f30
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2025-04-21 20:02:32 +0500

    adding separate functions for receiving & transmitting

--- src/usart_transaction.c
+++ src/usart_transaction.c
@@ -1,7 +1,6 @@
 #include "usart_transaction.h"
 
 
-
 HAL_Status_t usart_transaction_init(usart_transaction_t *trans, usart_transaction_cfg_t *cfg)
 {
     trans->host = cfg->host;
@@ -31,7 +30,9 @@ HAL_Status_t usart_transaction_init(usart_transaction_t *trans, usart_transactio
 
     if (cfg->direction == USART_TRANSACTION_TRANSMIT)
     {
-
+        trans->host->CONTROL3 |= UART_CONTROL3_DMAT_M;
+        trans->host->CONTROL1 |= UART_CONTROL1_TE_M;
+        while((trans->host->FLAGS & UART_FLAGS_TEACK_M) == 0);
         dma_cfg.read_mode = DMA_MEMORY_MODE;
         dma_cfg.write_mode = DMA_PERIPHERY_MODE;
         dma_cfg.read_increment = 1;
@@ -43,6 +44,9 @@ HAL_Status_t usart_transaction_init(usart_transaction_t *trans, usart_transactio
     }
     else if (cfg->direction == USART_TRANSACTION_RECEIVE)
     {
+        trans->host->CONTROL3 |= UART_CONTROL3_DMAR_M;
+        trans->host->CONTROL1 |= UART_CONTROL1_RE_M;
+        while((trans->host->FLAGS & UART_FLAGS_REACK_M) == 0);
         dma_cfg.read_mode = DMA_PERIPHERY_MODE;
         dma_cfg.write_mode = DMA_MEMORY_MODE;
         dma_cfg.read_increment = 0;
@@ -68,4 +72,42 @@ void RAM_ATTR usart_transaction_start(usart_transaction_t *trans, char *arr, uin
     else return;
     trans->dma_transaction.config.LEN = len;
     dma_transaction_start(&(trans->dma_transaction));
+}
+
+HAL_Status_t RAM_ATTR usart_transmit_start(usart_transaction_t *trans, const char *src, uint32_t len)
+{
+    if (trans->direction != USART_TRANSACTION_TRANSMIT)
+        return HAL_DMA_INCORRECT_ARGUMENT;
+    trans->dma_transaction.config.SRC = src;
+    trans->dma_transaction.config.LEN = len;
+    dma_transaction_start(&(trans->dma_transaction));
+    return HAL_DMA_OK;
+}
+
+HAL_Status_t RAM_ATTR usart_transmit(usart_transaction_t *trans, const char *src, uint32_t len, uint32_t timeout_us)
+{
+    HAL_Status_t res;
+    res = usart_transmit_start(trans, src, len);
+    if (res == HAL_DMA_OK)
+        res = usart_transaction_wait(trans, timeout_us);
+    return res;
+}
+
+HAL_Status_t RAM_ATTR usart_receive_start(usart_transaction_t *trans, char *dst, uint32_t len)
+{
+    if (trans->direction != USART_TRANSACTION_RECEIVE)
+        return HAL_DMA_INCORRECT_ARGUMENT;
+    trans->dma_transaction.config.DST = dst;
+    trans->dma_transaction.config.LEN = len;
+    dma_transaction_start(&(trans->dma_transaction));
+    return HAL_DMA_OK;
+}
+
+HAL_Status_t RAM_ATTR usart_receive(usart_transaction_t *trans, char *dst, uint32_t len, uint32_t timeout_us)
+{
+    HAL_Status_t res;
+    res = usart_receive_start(trans, dst, len);
+    if (res == HAL_DMA_OK)
+        res = usart_transaction_wait(trans, timeout_us);
+    return res;
 }
