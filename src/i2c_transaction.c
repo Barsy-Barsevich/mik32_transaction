fce32597da5995c6070ab30f5f05693c75beb9b8
Barsy-Barsevich <victor.kinderknecht7@gmail.com>
2025-04-22 11:08:56 +0500

    i2c transmitting dia dma works

--- src/i2c_transaction.c
+++ src/i2c_transaction.c
@@ -12,10 +12,8 @@ HAL_Status_t i2c_transaction_init(i2c_transaction_t *trans, i2c_transaction_cfg_
     HAL_Status_t res;
     trans->host = cfg->host;
     trans->direction = cfg->direction;
-    
-    // HAL_I2C_Master_SlaveAddress(hi2c, SlaveAddress);
-    /* Задать направление передачи - запись */
-    trans->host->CR2 &= ~I2C_CR2_RD_WRN_M;
+    trans->address = cfg->address;
+    trans->use_10bit_address = cfg->use_10bit_address;
 
     /* Разрешение поддержки DMA при передаче */
     trans->host->CR1 |= I2C_CR1_TXDMAEN_M;
@@ -59,14 +57,92 @@ HAL_Status_t i2c_transaction_init(i2c_transaction_t *trans, i2c_transaction_cfg_
         dma_cfg.read_ack_en = 1;
         dma_cfg.write_ack_en = 0;
         dma_cfg.src_address = (uint32_t)&(trans->host->RXDR);
-        dma_cfg.dst_address = (uint32_t)&(trans->host->TXDR);
+        dma_cfg.dst_address = 0;
     }
     else return HAL_DMA_INCORRECT_ARGUMENT;
     dma_cfg.transaction_len = 0;
     dma_transaction_init(&(trans->dma_transaction), &dma_cfg);
 
+    return HAL_DMA_OK;
+}
+
+HAL_Status_t RAM_ATTR i2c_transmit_start(i2c_transaction_t *trans, const char *src, uint8_t len)
+{
+    if (trans->direction != I2C_TRANSACTION_TRANSMIT)
+    return HAL_DMA_INCORRECT_ARGUMENT;
+
+    trans->dma_transaction.config.CFG |= DMA_CH_CFG_ENABLE_M;
+
+    /* write num of bytes */
+    trans->host->CR2 &= ~I2C_CR2_NBYTES_M;
+    trans->host->CR2 |= I2C_CR2_NBYTES(len);
+    /* autoend enable */
+    trans->host->CR2 &= ~I2C_CR2_AUTOEND_M;
+    trans->host->CR2 |= (1 << I2C_CR2_AUTOEND_S);
+
+    /* Writing address */
+    trans->host->CR2 &= ~I2C_CR2_SADD_M;
+    if (!trans->use_10bit_address) /* 7 битный адрес */
+    {
+        trans->host->CR2 &= ~I2C_CR2_ADD10_M;
+        trans->host->CR2 |= ((trans->address & 0x7F) << 1) << I2C_CR2_SADD_S;
+    }
+    else /* 10 битный адрес */
+    {
+        trans->host->CR2 |= I2C_CR2_ADD10_M;
+        /* ведущий отправляет полную последовательность для чтения для 10 битного адреса */
+        trans->host->CR2 &= ~I2C_CR2_HEAD10R_M;
+        trans->host->CR2 |= (trans->address & 0x3FF) << I2C_CR2_SADD_S;
+    }
+
+    /* Задать направление передачи - запись */
+    trans->host->CR2 &= ~I2C_CR2_RD_WRN_M;
+    /* Разрешение поддержки DMA при передаче */
+    trans->host->CR1 |= I2C_CR1_TXDMAEN_M;
+    /* Настройка и включение канала DMA */
+    trans->dma_transaction.config.SRC = (uint32_t)src;
+    trans->dma_transaction.config.LEN = len;
+    dma_transaction_start(&(trans->dma_transaction));
+
     /* Старт */
     trans->host->CR2 |= I2C_CR2_START_M;
 
     return HAL_DMA_OK;
+}
+
+
+HAL_Status_t i2c_transaction_end(i2c_transaction_t *trans, uint32_t timeout_us)
+{
+    HAL_Status_t res = dma_transaction_wait(&(trans->dma_transaction), timeout_us);
+    trans->dma_transaction.config.CFG &= ~DMA_CH_CFG_ENABLE_M;
+    if (res != HAL_DMA_OK) return res;
+    
+    /* Writing error statuses */
+    if (DMA_CONFIG->CONFIG_STATUS & (1 << (trans->dma_transaction.channel + DMA_STATUS_CHANNEL_BUS_ERROR_S)))
+        trans->status_dma_error = true;
+    else trans->status_dma_error = false;
+    uint32_t status_isr = trans->host->ISR;
+    uint8_t err = I2C_TRANSACTION_ERR_NONE;
+    if (status_isr & I2C_ISR_BERR_M) err |= I2C_TRANSACTION_ERR_BERR;
+    if (status_isr & I2C_ISR_ARLO_M) err |= I2C_TRANSACTION_ERR_ARLO;
+    if (status_isr & I2C_ISR_OVR_M)  err |= I2C_TRANSACTION_ERR_OVR;
+    trans->status_i2c_error = err;
+    return res;
+}
+
+
+void i2c_transaction_err_decode(i2c_transaction_t *trans)
+{
+    xprintf("%lu", HAL_Millis());
+    char tag[20];
+    xsprintf(tag, "%lu\tI2C transaction: ");
+
+    if (trans->status_dma_error) xprintf("%sI2C transaction: DMA err\n", tag);
+    i2c_transaction_err_t err = trans->status_i2c_error;
+    if (err == I2C_TRANSACTION_ERR_NONE) xprintf("%sI2C err: [NONE] (there aren't any error)\n", tag);
+    if (err & I2C_TRANSACTION_ERR_BERR) xprintf("%sI2C err: [BERR] (bus error)\n", tag);
+    if (err & I2C_TRANSACTION_ERR_ARLO) xprintf("%sI2C err: [ARLO] (the arbitrary fault)\n", tag);
+    if (err & I2C_TRANSACTION_ERR_OVR) xprintf("%sI2C err: [OVR] (data over/underloading error)\n", tag);
+    if (err & ~(I2C_TRANSACTION_ERR_BERR | I2C_TRANSACTION_ERR_ARLO | I2C_TRANSACTION_ERR_OVR))
+        xprintf("%sI2C err: [Unexpected] (unexpected error, code 0x%02X)\n", tag, err);
 }
--- src/i2c_transaction.h
+++ src/i2c_transaction.h
@@ -2,8 +2,9 @@
 
 #include "stdio.h"
 #include "stdbool.h"
-#include "hal_i2c.h"
+#include "mik32_hal_i2c.h"
 #include "dma_transaction.h"
+#include "xprintf.h"
 
 
 typedef enum
@@ -12,15 +13,22 @@ typedef enum
     I2C_TRANSACTION_RECEIVE = 1
 } i2c_transaction_direction_t;
 
+typedef enum
+{
+    I2C_TRANSACTION_ERR_NONE = 0,
+    I2C_TRANSACTION_ERR_BERR = (1<<0),
+    I2C_TRANSACTION_ERR_ARLO = (1<<1),
+    I2C_TRANSACTION_ERR_OVR  = (1<<2),
+} i2c_transaction_err_t;
+
 typedef struct
 {
     I2C_TypeDef *host;
     uint8_t dma_channel;
     uint8_t dma_priority;
     i2c_transaction_direction_t direction;
-    uint32_t src;
-    uint32_t dst;
     uint16_t address;
+    bool use_10bit_address;
 } i2c_transaction_cfg_t;
 
 typedef struct
@@ -28,4 +36,16 @@ typedef struct
     I2C_TypeDef *host;
     HAL_DMA_Transaction_t dma_transaction;
     i2c_transaction_direction_t direction;
+    uint16_t address;
+    bool use_10bit_address;
+    /* Error status can be read after i2c_transaction_end call */
+    bool status_dma_error;
+    i2c_transaction_err_t status_i2c_error;
 } i2c_transaction_t;
+
+HAL_Status_t i2c_transaction_end(i2c_transaction_t *trans, uint32_t timeout_us);
+
+HAL_Status_t i2c_transaction_init(i2c_transaction_t *trans, i2c_transaction_cfg_t *cfg);
+HAL_Status_t i2c_transmit_start(i2c_transaction_t *trans, const char *src, uint8_t len);
+
+void i2c_transaction_err_decode(i2c_transaction_t *trans);
